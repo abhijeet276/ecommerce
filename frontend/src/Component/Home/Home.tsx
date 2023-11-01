1f8daa766f1ee7ae53186494ee4fcee314ba50df
Aayu79 <Malhotraaayush5@gmail.com>
2023-11-01 23:24:08 +0530

    Add Header and Footer

---
+++ frontend/src/Component/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { CgMouse } from "react-icons"
+
+const Home:React.FC= () => {
+  return (
+    <div className='banner'>
+        <p>Welcome To Ecommerce</p>
+        <h1>FIND AMAZING PRODUCTS BELOW </h1>
+        <a href='#container'>
+            <button>
+                Scroll 
+            </button>
+        </a>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/Component/Layout/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import playStore from '../../../assets/logo/playstore.png'
+import appStore from '../../../assets/logo/Appstore.png'
+import "./Footer.css"
+function Footer() {
+  return (
+  <footer id = "footer">
+ <div className='leftFooter'>
+    <h4> DOWNLOAD OUR APP</h4>
+    <p> Download App for Android and IOS mobile Phone</p>
+<img src={playStore} alt='playstore' />
+<img src={appStore} alt='Appstore'/>
+ </div>
+ <div className='midFooter'>
+    <h1>ECOMMMERCE.</h1>
+    <p>High Quality is our first pripority</p>
+    <p>Copyrights 2023 &copy;</p>
+ </div>
+ <div className='rightFooter'>
+    <h4>Follow Us</h4>
+    <a href='https://www.instagram.com/'>Instagram</a>
+    <a href='https://www.youtube.com/'>Youtube</a>
+    <a href='https://www.facebook.com/'>FaceBook</a>
+ </div>
+  </footer>
+  )
+}
+
+export default Footer
