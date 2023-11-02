1cd49cb9b608a2ceb0b7db04b22dff84671229c1
Aayu79 <Malhotraaayush5@gmail.com>
2023-11-02 22:15:20 +0530

    Create Basic Layout of Home Page

--- frontend/src/Component/Home/index.tsx
+++ frontend/src/Component/Home/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 // import { CgMouse } from "react-icons"
-
+import './Home.css'
+import Product from '../Products'
 const Home:React.FC= () => {
   return (
+    <>
     <div className='banner'>
         <p>Welcome To Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW </h1>
@@ -12,6 +14,11 @@ const Home:React.FC= () => {
             </button>
         </a>
     </div>
+    <h2 className='homeHeading'>Featured Products</h2>
+    <div className='container ' id ='container'>
+    <Product product={product}/>
+    </div>
+    </>
   )
 }
 
